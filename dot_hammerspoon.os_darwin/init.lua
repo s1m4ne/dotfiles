061d3e158eb605bdf0370a40bba38310b1caa800
s1m4ne <121809222+s1m4ne@users.noreply.github.com>
2025-10-10 22:10:44 +0900

    reset

--- dot_hammerspoon.os_darwin/init.lua
+++
@@ -1,9 +0,0 @@
-function appWatcher(name, event, app)
-  if event == hs.application.watcher.activated then
-    if name == "Alacritty" or name == "ターミナル" then
-      if not hs.keycodes.currentSourceID("com.apple.keylayout.US") then
-        hs.keycodes.currentSourceID("com.apple.keylayout.ABC")
-      end
-    end
-  end end appWatcher = hs.application.watcher.new(appWatcher)
-appWatcher:start()
